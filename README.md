0501d5ee911c579c965ef2d492fcfe1c4b5d8ee6
HarKanJel <angel.r.guijarro@gmail.com>
2024-02-07 20:07:17 +0100

    Preprocesado y anonimización del dataset inicial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 09MIAR_TFM
 Aplicación de modelos LLM para extracción de datos en documentos de escritura pública.
+
+py -m venv .venv
+
+py -m pip install --upgrade pip
+
+py -m pip install PyPDF2, num2words, pandas, pyarrow, pyahocorasick, scikit-learn
---
+++ Source/globals.py
@@ -0,0 +1,17 @@
+import  os
+
+# Rutas globales
+# Directorio base donde se encuentran los DATASETS
+DATA_DIR = r"..\Dataset"
+
+# Directorio usado en el ayuntamiento para procesar los datos originales y generar los datasets anonimizados
+INITIAL_DIR = r"..\Dataset\Inicial"
+ESC_DIR = os.path.join(INITIAL_DIR,"escrituras")
+FICH_DIR = os.path.join(INITIAL_DIR,"fichas")
+PDF_FICHAS_DIR = os.path.join(INITIAL_DIR, "pdf_fichas")
+
+# Ficheros de diccionarios usados en la anonimización
+RANDOM_NOMBRES = os.path.join(DATA_DIR,"RANDOM","nombres.jsonl")
+RANDOM_APELLIDOS = os.path.join(DATA_DIR,"RANDOM","apellidos.jsonl")
+RANDOM_EMPRESAS = os.path.join(DATA_DIR,"RANDOM","empresas.jsonl")
+RANDOM_VIAS = os.path.join(DATA_DIR,"RANDOM","vias.jsonl")
