c0039d0d9694f67f834e67b5d64aea4e35ff16c9
HarKanJel <angel.r.guijarro@gmail.com>
2024-02-16 00:04:26 +0100

    Creando clases para el dataset NER

--- Dataset/ESCRITURAS/ESCRITURAS.py
+++ Dataset/ESCRITURAS/ESCRITURAS.py
@@ -4,22 +4,56 @@ import hashlib
 
 class ESCRITURAS(datasets.GeneratorBasedBuilder):
     BUILDER_CONFIGS = [
-        datasets.BuilderConfig(name="QA", description="Dataset para entrenamiento de modelos QA en extracción de datos de escrituras."),
-        datasets.BuilderConfig(name="NER", description="Dataset para entrenamiento de modelos NER en extracción de datos de escrituras.")
+        datasets.BuilderConfig(name="QA", \
+                               description="""Dataset para entrenamiento de modelos QA en extracción de datos de escrituras.
+                                    id:         Identificador único en el dataset para cada pregunta-contexto.
+                                    context:    Texto del documento donde se encuentra el registro.
+                                    question:   Texto de la pregunta que queremos resolver.
+                                        answers:Una lista de respuestas a esa pregunta que se extraen del contexto.
+                                            answer_start:   Caracter de inicio donde se encuentra la respuesta.
+                                            text:           Texto de la respuesta. 
+                                """),
+        datasets.BuilderConfig(name="NER", \
+                               description="""Dataset para entrenamiento de modelos NER en extracción de datos de escrituras.
+                                    Las etiquetas utilizadas se corresponden con los siguientes elementos:
+                                    'B-PROTO','I-PROTO':    Número de PROTOCOLO.
+                                    'B-FDOC','I-FDOC':      FECHA de firma del DOCUMENTO.
+                                    'B-NOT','I-NOT':        NOTARIO, nombre y apellidos.
+                                    'B-TDOC','I-TDOC':      TIPO de DOCUMENTO.
+                                """)
     ]
 
     def _info(self):
-        return datasets.DatasetInfo(
-            features=datasets.Features({
-                "id": datasets.Value("string"),
-                "context": datasets.Value("string"),
-                "question": datasets.Value("string"),
-                "answers": datasets.features.Sequence({
-                    "answer_start": datasets.Value("int32"),
-                    "text": datasets.Value("string"),
+        if self.config.name == "QA":
+            info = datasets.DatasetInfo(
+                features=datasets.Features({
+                    "id": datasets.Value("string"),
+                    "context": datasets.Value("string"),
+                    "question": datasets.Value("string"),
+                    "answers": datasets.features.Sequence({
+                        "answer_start": datasets.Value("int32"),
+                        "text": datasets.Value("string"),
+                    }),
                 }),
-            }),
-        )
+            )
+        elif self.config.name == "NER":
+            info = datasets.DatasetInfo(
+                description=self.config.description,
+                features=datasets.Features({
+                    "id": datasets.Value("string"),
+                    "tokens": datasets.Sequence(datasets.Value("sring")),
+                    "ner_tags": datasets.Sequence(datasets.featuresClassLabel(
+                        names=[
+                            'O',
+                            'B-PROTO','I-PROTO', # protocolo
+                            'B-FDOC','I-FDOC', # fecha
+                            'B-NOT','I-NOT', # notario
+                            'B-TDOC','I-TDOC', # tipo documento
+                        ]
+                    ))
+                })
+            )
+        return info
 
 
     DATA_URLS = {
@@ -47,21 +81,28 @@ class ESCRITURAS(datasets.GeneratorBasedBuilder):
     def _generate_examples(self, filepath):
         with open(filepath, encoding="utf-8") as f:
             json_data = json.load(f)
-            for d in json_data['data']:
-                    for escritura in d['paragraphs']:                    
-                        id = escritura['id']
-                        context = escritura['context']
-                        for qa in escritura['qas']:
-                            question = qa['question']
-                            answers = qa['answers']
-                            uid = f"{id}_{hashlib.sha256(question.encode('utf-8')).hexdigest()}"                    
-                            yield uid, {
-                                "id": uid,
-                                "context": context,
-                                "question": question,
-                                "answers": {
-                                    "answer_start": [answer["answer_start"] for answer in answers],
-                                    "text": [answer["text"] for answer in answers]
-                                },
-                            }
+            if self.config.name == "QA":
+                for d in json_data['data']:
+                        for escritura in d['paragraphs']:                    
+                            id = escritura['id']
+                            context = escritura['context']
+                            for qa in escritura['qas']:
+                                question = qa['question']
+                                answers = qa['answers']
+                                uid = f"{id}_{hashlib.sha256(question.encode('utf-8')).hexdigest()}"                    
+                                yield uid, {
+                                    "id": uid,
+                                    "context": context,
+                                    "question": question,
+                                    "answers": {
+                                        "answer_start": [answer["answer_start"] for answer in answers],
+                                        "text": [answer["text"] for answer in answers]
+                                    },
+                                }
+            elif self.config.name == "NER":
+                for escritura in json_data:
+                    id = escritura['id']
+                    t = escritura['tokens']
+                    nt = escritura['ner_tags']
+                    yield id, {'id': id, 'tokens': t, 'ner_tags': nt}
 
