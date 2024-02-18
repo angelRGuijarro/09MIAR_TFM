72808cebf4f0f3451272a0e8e644447e4d636475
HarKanJel <angel.r.guijarro@gmail.com>
2024-02-18 04:34:09 +0100

    Entrenamiento NER y evaluación final

--- Source/metrics/evaluar_metricas.py
+++ Source/metrics/evaluar_metricas.py
@@ -1,5 +1,12 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from transformers import (
+    AutoModelForQuestionAnswering,
+    AutoModelForTokenClassification, 
+    AutoTokenizer, 
+    pipeline
+    )
+import datasets
 from .evaluate import compute_exact, compute_f1
+from seqeval.metrics import f1_score, precision_score, recall_score,accuracy_score
 import torch
 import mlflow
 from statistics import mean
@@ -44,5 +51,42 @@ def evaluar_metricas_QA(model_path_or_name, dataset, batch_size=512, split='test
     print('\tf1:', f1_mean)
     print('\texact:', exact_mean)
 
-def evaluar_metricas_NER(model_path_or_name, dataset, batch_size=512):
-    pass
+def evaluar_metricas_NER(model_path_or_name, dataset, batch_size=64,split='test'):
+    """Escribe por pantalla y guarda en MLflow los valores de las métricas f1 y exact match para este modelo y dataset."""
+    model = AutoModelForTokenClassification.from_pretrained(model_path_or_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_path_or_name)
+    token_inputs = tokenizer(dataset['tokens'], is_split_into_words=True, truncation=True, padding=True)
+
+    lista_etiquetas = dataset.features['ner_tags'].feature.names    
+    dataset = dataset.map(lambda x: {'labels': [lista_etiquetas[label_id] for label_id in x['ner_tags']],
+                                     'texts': " ".join(x['tokens'])})
+    labels = dataset['labels']
+
+    consulta =  pipeline("ner", model=model, tokenizer=tokenizer, 
+                device=0 if torch.cuda.is_available() else None, batch_size=batch_size)        
+    predicciones = consulta(dataset['texts'])
+    # Para obtener las predicciones el el formato esperado
+    predictions_list = []    
+    for i, pred in enumerate(predicciones):
+        word_ids = token_inputs.word_ids(batch_index=i)
+        # Genero una lista del tamaño de las etiquetas
+        pred_total = ["O"]*len(labels[i])
+        # Y cambio su valor para cada predicción encontrada
+        for p in pred:
+            pred_total[word_ids[p['index']]]=p['entity']
+        predictions_list.append(pred_total)
+
+    f1 =        f1_score(labels, predictions_list)
+    precision = precision_score(labels, predictions_list)
+    recall =    recall_score(labels, predictions_list)
+    accuracy =  accuracy_score(labels, predictions_list)
+    
+    
+    mlflow.log_param('dataset_split', split)
+    mlflow.log_param('model_name',model.name_or_path)
+    mlflow.log_metric('f1_score', f1)
+    mlflow.log_metric('precision', precision)
+    mlflow.log_metric('recall', recall)
+    mlflow.log_metric('accuracy', accuracy)
+    print(model.name_or_path)
+    print('\tf1:', f1)
